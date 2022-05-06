d2a016147ad4d757961a184d689934be0556a79c
Furkan Cebeci <funcexi@gmail.com>
2022-05-06 18:49:40 +0300

    first commit

---
+++ src/LeapYearCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the year: ");
+        year = input.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println(year + " is a leap year!");
+        } else if (year % 400 == 0) {
+            System.out.println(year + " is a leap year!");
+        } else {
+            System.out.println(year + " is not a leap year!");
+        }
+    }
+}
